ed48af94447588bf92ccbdfe3a61be98169087b5
JoaoH2512 <161508350+JoaoH2512@users.noreply.github.com>
2026-09-23 17:39:39 -0300

    atualizando o banco com livros

--- database/biblioteca_db.sql
+++ database/biblioteca_db.sql
@@ -1,41 +1,72 @@
 -- phpMyAdmin SQL Dump
--- Banco de dados: biblioteca_db
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23/09/2026 às 22:39
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS `biblioteca_db`
-    DEFAULT CHARACTER SET utf8mb4
-    COLLATE utf8mb4_general_ci;
 
-USE `biblioteca_db`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca_db`
+--
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `livros`
+-- Estrutura para tabela `livros`
 --
 
-CREATE TABLE IF NOT EXISTS `livros` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `titulo` varchar(150) NOT NULL,
-    `autor` varchar(120) NOT NULL,
-    `ano_publicacao` int NOT NULL,
-    `disponivel` tinyint(1) NOT NULL DEFAULT 1,
-    PRIMARY KEY (`id`),
-    KEY `ix_livros_id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+CREATE TABLE `livros` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `autor` varchar(120) NOT NULL,
+  `ano_publicacao` int(11) NOT NULL,
+  `disponivel` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dados da tabela `livros`
+-- Despejando dados para a tabela `livros`
 --
 
-INSERT INTO `livros`
-    (`titulo`, `autor`, `ano_publicacao`, `disponivel`)
-VALUES
-    ('O Hobbit', 'J. R. R. Tolkien', 1937, 1);
+INSERT INTO `livros` (`id`, `titulo`, `autor`, `ano_publicacao`, `disponivel`) VALUES
+(1, 'O Hobbit', 'J. R. R. Tolkien', 1937, 1),
+(2, 'Assim que Acaba', 'Colen Holver', 2019, 1),
+(3, 'Verity', 'Colen Holver', 2024, 1),
+(4, 'Harry Potter', 'J. K. Rowling', 2009, 1);
 
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ix_livros_id` (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
